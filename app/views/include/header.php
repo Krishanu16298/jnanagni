725ba6e1a14fdf8f3e198120ea18bdae634f7672
Krishanu Mallik Thakur <mkrishanu98@gmail.com>
2018-08-08 14:05:17 +0530

    Initial Commit

--- app/views/include/header.php
+++ app/views/include/header.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv='cache-control' content='no-cache'>
+    <meta http-equiv='expires' content='0'>
+    <meta http-equiv='pragma' content='no-cache'>
     <link rel="stylesheet" href="<?php echo URLROOT;?>/css/style.css">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://fonts.googleapis.com/css?family=Do+Hyeon|Indie+Flower|Lobster" rel="stylesheet">
